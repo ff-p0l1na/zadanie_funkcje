683c944809bf8bb61675ec06eba000e4781a4d79
Paulina F <faltynp@gmail.com>
2023-03-02 13:02:23 +0100

    dodawanie userow dziala

--- main-szkola.py
+++ main-szkola.py
@@ -3,28 +3,27 @@ class Szkola:
         self.klasy = {}
         self.nauczyciele = {}
 
+    def wez_klase(self, nazwa_klasy):
+        if nazwa_klasy not in self.klasy:
+            self.dodaj_nowa_klase(nazwa_klasy)
+        return self.klasy[nazwa_klasy]
+
+    def dodaj_nauczyciela(self,nauczyciel):
+        self.nauczyciele[nauczyciel.dane] = nauczyciel
+
     def dodaj_nowa_klase(self, klasa):
         if klasa in self.klasy:
             return
-        self.klasy[klasa] = self.pusta_klasa()
+        self.klasy[klasa] = self.pusta_klasa(klasa)
 
-    def dodaj_nauczyciela(self, nauczyciel):
-        self.nauczyciele[nauczyciel.dane] = nauczyciel
-
-    def pusta_klasa(self):
+    def pusta_klasa(self, klasa):
         return {
+            "nazwa": klasa,
             "uczniowie": [],
             "wychowawca": [],
             "nauczyciele": [],
         }
 
-
-class Klasa:
-    def __init__(self, nazwa):
-        self.nazwa = nazwa
-        self.uczniowie = []
-        self.nauczyciele = []
-        self.wychowawca = None
 
 class Uczen:
     def __init__(self, dane, klasa):
@@ -32,30 +31,21 @@ class Uczen:
         self.klasa = klasa
         self.nauczyciele = []
 
-    def pokaz_dane_ucznia(self):
-        print(f"Imię i nazwisko ucznia to {Uczen.dane}. ")
-
-    def pokaz_klase_ucznia(self):
-        print(f"Uczeń chodzi do klasy {Uczen.klasa}. ")
-
-    def wypisz_nauczycieli(self):
-        print(f)
+    def __repr__(self):
+        return f'({self.dane}, {self.klasa})'
 
 
 class Nauczyciel:
-    def __init__(self, dane, przedmiot, klasy=[]):
+    def __init__(self, dane, przedmiot, klasy=None):
         self.dane = dane
         self.przedmiot = przedmiot
-        self.klasy = klasy
+        if klasy:
+            self.klasy = klasy
+        else:
+            self.klasy = []
 
-    def pokaz_dane_nauczyciela(self):
-        print(f"Imię i nazwisko nauczyciela to {Nauczyciel.dane}.")
-
-    def pokaz_nauczany_przedmiot(self):
-        print(f"{Nauczyciel.dane} uczy przedmiotu o nazwie {Nauczyciel.przedmiot}.")
-
-    def pokaz_klasy_nauczyciela(self):
-        print(f"{Nauczyciel.dane} uczy w następujących klasach: {Nauczyciel.klasa}")
+    def __repr__(self):
+        return f'({self.dane}, {self.przedmiot})'
 
 
 class Wychowawca:
@@ -63,11 +53,8 @@ class Wychowawca:
         self.dane = dane
         self.klasa = klasa
 
-    def pokaz_dane_wychowawcy(self):
-        print(f"Imię i nazwisko wychowawcy to {Wychowawca.dane}.")
-
-    def pokaz_klase_wychowawcy(self):
-        print(f"{Wychowawca.dane} jest wychowawcą w klasie {Wychowawca.klasa}.")
+    def __repr__(self):
+        return f'({self.dane}, {self.klasa})'
 
 
 def wczytaj_ucznia():
@@ -77,12 +64,18 @@ def wczytaj_ucznia():
     return uczen
 
 
-def wczytaj_nauczyciela():
+def wczytaj_nauczyciela(szkola):
     dane = input("Podaj imię i nazwisko: \n")
     przedmiot = input("Podaj nazwę przedmiotu: \n")
-    klasa = input("Podaj klasę: \n")
-    nauczyciel = Nauczyciel(dane, przedmiot, klasy.append(klasa))
-    return nauczyciel
+    nauczyciel = Nauczyciel(dane, przedmiot)
+    szkola.dodaj_nauczyciela(nauczyciel)
+    while True:
+        nazwa_klasy = input("Podaj klasę. (Naciśnij [enter] żeby zakończyć wpisywanie klas.) \n")
+        if not nazwa_klasy:
+            break
+        klasa = szkola.wez_klase(nazwa_klasy)
+        klasa['nauczyciele'].append(nauczyciel)
+        nauczyciel.klasy.append(klasa)
 
 
 def wczytaj_wychowawce():
@@ -110,15 +103,20 @@ while True:
             continue
         elif tworzony_typ == "uczeń":
             uczen = wczytaj_ucznia()
-            klasa_ucznia = Klasa(uczen.klasa)  # Klasa(nazwa)
-            klasa_ucznia.uczniowie.append(uczen.dane)
-            szkola.dodaj_nowa_klase(klasa_ucznia.nazwa)  # dodaj_nowa_klase(self, klasa)
-            # jak dodac stworzony obiekt "klasa" do "szkoly" i porozdzielac zawarte w obiekcie info
-            # na czytelne formy
-            # przypadek jak klasa, np "1a" juz istnieje i chcemy tylko dopisac ucznia
-            # przypadek jak otwieramy klase od nowa
-            # przypadek jak otwieramy klase z poziomu wychowawcy, bo jego tez mozna stworzyc dla
-            # jeszcze nie istniejacej klasy
+            klasa = szkola.wez_klase(uczen.klasa)
+            klasa['uczniowie'].append(uczen)
+        elif tworzony_typ == "wychowawca":
+            wychowawca = wczytaj_wychowawce()
+            klasa = szkola.wez_klase(wychowawca.klasa)
+            klasa['wychowawca'] = wychowawca
+        elif tworzony_typ == "nauczyciel":
+            wczytaj_nauczyciela(szkola)
+    elif polecenie == "zarządzaj":
+        pass
+    elif polecenie == "koniec":
+        break
+
+
 
 
 
